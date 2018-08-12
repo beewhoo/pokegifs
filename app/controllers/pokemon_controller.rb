4c3bad73eef70053c1cfc17d6a0699adf6475217
beewhoo <bibeksinghtthapa@gmail.com>
2018-08-11 23:57:15 -0400

    controller makes two calls one for pokemon and another for giphy, added css to visualize json

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,33 +1,72 @@
 class PokemonController < ApplicationController
 
-    def show
+  def index
+    key = 'iEF6f6dAt0G79yfdH06TAndtrQ8wk6zO&q'
 
-      #poke API
-    pokemon_response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    pokemon_response = HTTParty.get("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
     pokemon_body = JSON.parse(pokemon_response.body)
-    pokemon_name  = pokemon_body["name"]
-    pokemon_id  = pokemon_body["id"]
 
-    types =   pokemon_body["types"]
 
-    pokemon_type = []
-    types.each do |type|
-      pokemon_type << type['type']['name']
+    @pokemon_image        = pokemon_body['pokemon'].first['img']
+    @pokemon_id           = pokemon_body['pokemon'].first["id"]
+    @pokemon_name         = pokemon_body['pokemon'].first["name"]
+    @pokemon_type         = pokemon_body['pokemon'].first["type"]
+    @pokemon_height       = pokemon_body['pokemon'].first["height"]
+    @pokemon_weight       = pokemon_body['pokemon'].first["weight"]
+    @pokemon_candy        = pokemon_body['pokemon'].first["candy"]
+    @pokemon_spawn_chance = pokemon_body['pokemon'].first["spawn_chance"]
+    @pokemon_avg_spawns   = pokemon_body['pokemon'].first["avg_spawns"]
+    @pokemon_spawn_time   = pokemon_body['pokemon'].first["spawn_time"]
+    @pokemon_multipliers  = pokemon_body['pokemon'].first["multipliers"]
+    @pokemon_weaknesses   = pokemon_body['pokemon'].first["weaknesses"]
+
+    gif_response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{@pokemon_name}&rating=g")
+    gif_body = JSON.parse(gif_response.body)
+    @gif_id = gif_body['data'].first['id']
+
+
+    if params[:term]
+
+      pokemon_body['pokemon'].each do |poke|
+
+      if poke['name'].downcase == params[:term]
+
+      @pokemon_image         = poke['img']
+      @pokemon_id           = poke["id"]
+      @pokemon_name         = poke["name"]
+      @pokemon_type         = poke["type"]
+      @pokemon_height       = poke["height"]
+      @pokemon_weight       = poke["weight"]
+      @pokemon_candy        = poke["candy"]
+      @pokemon_spawn_chance = poke["spawn_chance"]
+      @pokemon_avg_spawns   = poke["avg_spawns"]
+      @pokemon_spawn_time   = poke["spawn_time"]
+      @pokemon_multipliers  = poke["multipliers"]
+      @pokemon_weaknesses   = poke["weaknesses"]
+
+
+      gif_response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{@pokemon_name}&rating=g")
+      gif_body = JSON.parse(gif_response.body)
+      @gif_id = gif_body['data'].first['id']
+
     end
 
-    #giphy API
-    gif_response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=pikachu&rating=g")
-    gif_body = JSON.parse(gif_response.body)
-    gif_url = gif_body["data"].sample["url"]
 
-    # render json
-    render json: {
-      "id": pokemon_id,
-      "name": pokemon_name,
-      "types": pokemon_type,
-      "gif": gif_url
-    }
-
+    end
   end
+
+end
+
+
+
+
+
+
+
+
+
+    def show
+
+    end
 
 end
