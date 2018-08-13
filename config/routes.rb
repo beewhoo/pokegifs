6b5a8445e60ca79e8fdf6aa56f5989cd9b1b049c
beewhoo <bibeksinghtthapa@gmail.com>
2018-08-12 22:48:53 -0400

    gotta catchem all

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :pokemon, only: %i(show index)
+resources :pokemon, only: %i(index)
+
+get '/', to: 'pokemon#index'
 end
